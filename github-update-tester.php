2bafdfd4c346cc848adf0acdfdfdb945ed6474e3
Seth Carstens <seth.carstens@gmail.com>
2016-09-21 12:12:23 -0700

    Create github-update-tester.php

---
+++ github-update-tester.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Github Updater Tester
+ * Plugin URI: https://github.com/fansided/github-updater-tester
+ * Description: Nothing fancy, but lots of releases going to be on this plugin!
+ * Author: sethcarstens
+ * Version: 0.0.1
+ * Author URI: http://sethcarstens.com/wordpress
+ * License: GPL V3
+ * Text Domain: ghu
+ *
+ * GitHub Plugin URI: https://github.com/fansided/github-updater-tester
+ * GitHub Branch: master
+ *
+ * @package fs_vip
+ * @category plugin
+ * @author FanSided <techteam@fansided.com>
+ */
+ 
+ //TODO: might be a good idea to allow this plugin to have a 
+ // settings page the prints debug info about GHU data stored in the DB!!
+ 
