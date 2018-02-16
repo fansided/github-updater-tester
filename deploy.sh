c45760bc10bd61c0f744515b00c5abb42bc87c6f
Stephen Greer <info@stephengreer.me>
2018-02-16 12:43:59 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ SSH_HOST=$2
 PLUGIN_PATH=$3
 
 echo "Transferring files to temp directory on server..."
-rsync -aze --force --delete --progress --exclude 'node_modules' --exclude '.git/' "ssh -o StrictHostKeyChecking=no" ./ $SSH_USER@$SSH_HOST:$PLUGIN_PATH-temp
+ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST
+rsync -az --force --delete --progress --exclude 'node_modules' --exclude '.git/' ./ $SSH_USER@$SSH_HOST:$PLUGIN_PATH-temp
 
 echo "Transfer complete. Deploying files to production..."
 ssh $SSH_USER@$SSH_HOST "mv -f $PLUGIN_PATH-temp $PLUGIN_PATH"
